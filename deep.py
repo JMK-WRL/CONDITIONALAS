c18e3198bb0d233015c0b83e15336d9753c7357e
JMK-WRL <121217004+JMK-WRL@users.noreply.github.com>
2023-02-09 20:32:37 +0300

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,5 @@
+answer = input("what is the answer to the Great Question of Life, the Universe, and Everything? ")
+if answer.lower() in ["42", "forty two", "forty-two"]:
+    print("Yes")
+else:
+    print("No")
